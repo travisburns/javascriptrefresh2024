651c376e6c0a5d5913ac8ac2eac4250f05ef5842
travis <travisjb0@gmail.com>
2024-01-17 15:52:36 -0800

    Variable types, const, let, var

--- startingfiles/script.js
+++ startingfiles/script.js
@@ -3,3 +3,17 @@
 
 // console.error('this is an error');
 // console.warn('THis is a warning');
+
+
+
+//Variables and how to set them 
+
+//var, es6: let: reasign values, const: constant value
+
+let age = 30;
+age = 31; 
+
+console.log(age)
+
+
+
